7a310dc6f7fb5a20b51ab6184c65afd0bd1ce3ba
sachin-yernale <sachin.ernaly@lonartech.com>
2021-06-17 18:41:17 +0530

    url

--- src/app/layout/user-data/userdata.service.ts
+++ src/app/layout/user-data/userdata.service.ts
@@ -9,20 +9,20 @@ export class UserdataService {
   URL: string;
 
   constructor(private httpClient:HttpClient) {
-    this.URL= 'http://af336c890cd1a4c1eae36f972d556914-1183209814.us-east-1.elb.amazonaws.com:8080/';
+    this.URL= "http://af336c890cd1a4c1eae36f972d556914-1183209814.us-east-1.elb.amazonaws.com:8080/";
    }
 
   getAll() {
-    return this.httpClient.get(this.URL+'getAllProducts')
+    return this.httpClient.get(this.URL+"getAllProducts")
     
   }
   saveProduct(data){
-    return this.httpClient.post(this.URL+'addProduct',data)
+    return this.httpClient.post(this.URL+"addProduct",data)
   }
   deleteProduct(data){
     return this.httpClient.delete(this.URL+'deleteProduct',data)
   }
   updateProduct(data){
-    return this.httpClient.put( this.URL+'updateProduct',data)
+    return this.httpClient.put( this.URL+"updateProduct",data)
   }
 }
